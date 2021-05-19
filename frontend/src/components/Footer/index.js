13b9819d0da81ad949a34adadc960a82180cc49d
SerraZ3 <serra.henrique3@gmail.com>
2021-05-19 16:46:15 -0300

    feature: criado footer e adicionado ao Home

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p class="footer__copy">
+        Sequelize #02 - Models & Queries &copy; | {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,45 @@
 import React from "react";
-
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 // import { Container } from './styles';
 
 function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section class="main-section">
+          <h2 class="main-section__title">Página Inicial</h2>
+          <h3 class="main-section__subtitle">
+            Bem vindo à prática de Sequelize #02!
+          </h3>
+          <p class="main-section__description">
+            Nesse repositório criaremos um projeto simples, onde poderemos
+            criar, consultar, editar e excluir usuários a partir de um banco de
+            dados MySQL.
+          </p>
+          <p class="main-section__description">
+            O intuito é entendermos como conectar o backend (node.js) a um banco
+            de dados, realizar o CRUD (Create, Read, Update e Delete) através do
+            Sequelize e utilizarmos queries SQL baseadas em modelos para
+            executarmos as ações (Model Queries).
+          </p>
+        </section>
+        <div class="cta">
+          <a
+            href="/users"
+            rel="next"
+            target="_self"
+            title="Ver listagem de usuários"
+            class="cta__btn"
+          >
+            Ver Lista de Usuários
+          </a>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
